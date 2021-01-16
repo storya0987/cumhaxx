0b35ad7451ffe51f14de6a5df32b972eb24d6e26
Yuh <75444356+yuhsecurity@users.noreply.github.com>
2021-01-16 02:46:33 -0600

    Updated Loadstrings

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 -- CumHaxx Loader by yuhsecurity
 -- CumHaxx Scripts by  ascaxz on github | https://github.com/ascaxz
 if game.PlaceId == 292439477 then -- Phantom Forces
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/Phantom%20Forces"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/Phantom%20Forces.lua"))()
 elseif game.PlaceId == 3233893879 then -- Bad Business
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/Bad%20Business"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/Bad%20Business.lua"))()
 end
